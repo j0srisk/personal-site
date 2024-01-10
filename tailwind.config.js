fcbc625c94616cfc209d4ab3a1c31839f0b0d9cc
j0srisk <j0srisk@gmail.com>
2024-01-10 12:56:37 -0600

    cleaned up buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			boxShadow: {
+				'inner-button': 'inset 0 1px 1px 0 hsla(0, 0%, 100%, 0.5)'
+			}
+		}
+	},
+	plugins: []
+}
