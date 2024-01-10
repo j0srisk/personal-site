875d9dbf2d44265acb665a010789b7b1131637cc
j0srisk <j0srisk@gmail.com>
2024-01-10 13:23:00 -0600

    added stats component

---
+++ src/components/Statsbox.tsx
@@ -0,0 +1,8 @@
+export default function Statsbox() {
+	return (
+		<div className="flex h-full w-full flex-col items-center justify-center text-indigo-900">
+			<p className="text-5xl font-semibold">8 +</p>
+			<p className="font-semibold">Years Freelancing</p>
+		</div>
+	)
+}
