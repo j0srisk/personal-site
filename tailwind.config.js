c0103e99af0cb3f74f65c53b82cd5b6b5af5da09
j0srisk <j0srisk@gmail.com>
2024-01-12 11:43:57 -0600

    added inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['Inter Variable', 'Inter', ...defaultTheme.fontFamily.sans],
+				roboto: ['Roboto Mono', ...defaultTheme.fontFamily.mono]
+			},
 			boxShadow: {
 				'inner-button': 'inset 0 1px 1px 0 hsla(0, 0%, 100%, 0.5)'
 			}
